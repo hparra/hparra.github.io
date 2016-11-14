6e711fef8b567f72735309c3cbe8720a988b98ec
HGPA <hector@hectorparra.com>
2016-11-13 22:09:07 -0800

    Fix travis

--- .gliki/deploy.sh
+++ .gliki/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # relative to root
 # FIXME: spin up another server that uses a key (why is this no the case already???)
-sshpass -p $PASSWORD rsync -vrltz -e ssh ./.gliki/build/ hgpa@hectorparra.com:/var/www/hectorparra.com/html/
+sshpass -p $PASSWORD rsync -vrltz -e 'ssh -o StrictHostKeyChecking=no' ./.gliki/build/ hgpa@hectorparra.com:/var/www/hectorparra.com/html/
