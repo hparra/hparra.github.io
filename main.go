0e42b8e8a0625766fc125c34ccc3342bb246989d
Hayley Parra <h@hgpa.tv>
2022-10-17 12:20:22 -0700

    Got ASTTransformer plugged in

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
@@ -11,14 +12,45 @@ import (
 	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/yuin/goldmark"
 	"github.com/yuin/goldmark-meta"
+	"github.com/yuin/goldmark/ast"
 	"github.com/yuin/goldmark/extension"
 	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/text"
+	"github.com/yuin/goldmark/util"
 )
 
 // Document represents a goliki file.
+// It contains the file data and metadata at multiple stages in the pipeline.
 type Document struct {
-	File       *object.File
-	LastCommit *object.Commit
+	// File written in markdown
+	File *object.File
+	// HTML rendered from File
+	MarkdownHTML *bytes.Buffer
+	LastCommit   *object.Commit
+}
+
+// LinkTransformer implements ASTTransformer.
+// https://pkg.go.dev/github.com/yuin/goldmark@v1.5.2/parser?utm_source=gopls#ASTTransformer
+type LinkTransformer struct{}
+
+// Transform transforms the provided Markdown AST.
+func (*LinkTransformer) Transform(doc *ast.Document, reader text.Reader, pctx parser.Context) {
+
+	ast.Walk(doc, func(node ast.Node, enter bool) (ast.WalkStatus, error) {
+		if !enter {
+			return ast.WalkContinue, nil
+		}
+
+		cb, ok := node.(*ast.Link)
+		if !ok {
+			return ast.WalkContinue, nil
+		}
+
+		// TODO: transform Destination url
+		fmt.Println(string(cb.Destination))
+
+		return ast.WalkContinue, nil
+	})
 }
 
 func main() {
@@ -88,6 +120,11 @@ func main() {
 
 	// create configured goldmark
 	markdown := goldmark.New(
+		goldmark.WithParserOptions(
+			parser.WithASTTransformers(
+				util.Prioritized(&LinkTransformer{}, 100),
+			),
+		),
 		goldmark.WithExtensions(
 			extension.GFM,
 			meta.Meta,
@@ -95,18 +132,10 @@ func main() {
 	)
 
 	// for each doc we need to calculate the resulting path
-	// e.g. f/a.md > build/f/a.html
-	// and write rendered contents here
+
+	// for each doc parse it's markdown and store rendered HTML
+	// TODO: store YAML front matter for each
 	for _, d := range docs {
-
-		//
-		relpath := strings.Replace(d.File.Name, ".md", ".html", 1)
-		buildpath := path.Join("build", relpath)
-
-		fmt.Println(buildpath)
-
-		// mkdir -p
-		os.MkdirAll(path.Dir(buildpath), os.ModePerm)
 
 		// get file content
 		contents, err := d.File.Contents()
@@ -115,19 +144,44 @@ func main() {
 			continue
 		}
 
+		// Why do I have to do this?
+		d.MarkdownHTML = new(bytes.Buffer)
+
+		// TODO: Custom Link renderer
+		// TODO: Add https://github.com/yuin/goldmark-highlighting
+		context := parser.NewContext()
+		if err := markdown.Convert([]byte(contents), d.MarkdownHTML, parser.WithContext(context)); err != nil {
+			panic(err)
+		}
+	}
+
+	// TODO: process site-wide metadata from all front matter
+
+	// for each doc we want to render the specified layout with metadata and generated markdown,
+	// and write the final content to file.
+	for _, d := range docs {
+		// TODO: get YAML front matter and read layout (template)
+		// TODO: render layout with metadata and markdown
+
+		//
+		// write HTML file to file path
+		//
+
+		// e.g. f/a.md > build/f/a.html
+		relpath := strings.Replace(d.File.Name, ".md", ".html", 1)
+		buildpath := path.Join("build", relpath)
+		fmt.Println(buildpath)
+
+		// mkdir -p
+		os.MkdirAll(path.Dir(buildpath), os.ModePerm)
+
 		f, err := os.Create(buildpath)
 		if err != nil {
 			panic(err)
 		}
 		defer f.Close()
 
-		// TODO: Custom Link renderer
-		// TODO: Add https://github.com/yuin/goldmark-highlighting
-		context := parser.NewContext()
-		if err := markdown.Convert([]byte(contents), f, parser.WithContext(context)); err != nil {
-			panic(err)
-		}
-
+		f.Write(d.MarkdownHTML.Bytes())
 	}
 
 }
