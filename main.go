71250a1552e95b9020d19ec93d1c34f7e9d29f88
Hayley Parra <h@hgpa.tv>
2022-10-17 17:25:17 -0700

    [goliki] Working default layout

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"path"
 	"strings"
+	"text/template"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing/object"
@@ -24,7 +25,8 @@ import (
 // It contains the file data and metadata at multiple stages in the pipeline.
 type Document struct {
 	// File written in markdown
-	File *object.File
+	File     *object.File
+	Metadata map[string]interface{}
 	// HTML rendered from File
 	MarkdownHTML *bytes.Buffer
 	LastCommit   *object.Commit
@@ -162,21 +164,25 @@ func main() {
 		// Why do I have to do this?
 		d.MarkdownHTML = new(bytes.Buffer)
 
-		// TODO: Custom Link renderer
 		// TODO: Add https://github.com/yuin/goldmark-highlighting
 		context := parser.NewContext()
 		if err := markdown.Convert([]byte(contents), d.MarkdownHTML, parser.WithContext(context)); err != nil {
 			panic(err)
 		}
+		// TODO: Metadata is too raw
+		d.Metadata = meta.Get(context)
+		// fmt.Println(d.Metadata)
 	}
 
 	// TODO: process site-wide metadata from all front matter
+
+	// FIXME
+	t := template.Must(template.New("default.html").ParseFiles(".goliki/layouts/default.html"))
 
 	// for each doc we want to render the specified layout with metadata and generated markdown,
 	// and write the final content to file.
 	for _, d := range docs {
 		// TODO: get YAML front matter and read layout (template)
-		// TODO: render layout with metadata and markdown
 
 		//
 		// write HTML file to file path
@@ -184,7 +190,10 @@ func main() {
 
 		// e.g. f/a.md > build/f/a.html
 		relpath := strings.Replace(d.File.Name, ".md", ".html", 1)
-		buildpath := path.Join("build", relpath)
+
+		// FIXME: default var or parameter
+		buildpath := path.Join(".goliki/public/", relpath)
+
 		fmt.Println(buildpath)
 
 		// mkdir -p
@@ -196,7 +205,13 @@ func main() {
 		}
 		defer f.Close()
 
-		f.Write(d.MarkdownHTML.Bytes())
+		// render template with doc as context and write to file
+		err = t.Execute(f, d)
+		if err != nil {
+			panic(err)
+		}
+
+		// f.Write(d.MarkdownHTML.Bytes())
 	}
 
 }
