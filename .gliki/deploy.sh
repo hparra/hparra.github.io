db8b271ca4852fd40cde9351b7106f6cde1aba18
Hayley Parra <h@hgpa.tv>
2022-10-20 20:15:32 -0700

    Delete gliki-related files

--- .gliki/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# relative to root
-# FIXME: spin up another server that uses a key (why is this no the case already???)
-sshpass -p $PASSWORD rsync -vrltz -e 'ssh -o StrictHostKeyChecking=no' ./.gliki/build/ hgpa@hectorparra.com:/var/www/hectorparra.com/html/
